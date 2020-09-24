72cb5f2fa41e479db6f1798c9d3a9b614f80cb6b
Miguel Solano <miguel@buildmidwestern.com>
2020-09-24 13:59:22 -0500

    [fix] resolves implementation of factory pattern

--- spaces/groupSpace.php
+++ spaces/groupSpace.php
@@ -6,6 +6,11 @@ use Src\Space;
 
 class GroupSpace implements Space
 {
+    public function getType()
+    {
+        return 'group';
+    }
+
     public function log()
     {
         echo '<center><h1>This is a Group Space for Group Conversations</h1></center>';
